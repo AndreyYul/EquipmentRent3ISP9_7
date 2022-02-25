3a0b56407a65fbf7179c4e4cc3ec65503ec2f430
Андрей Юлин <andrew25555@yandex.ru>
2022-02-25 12:44:37 +0300

    88888

--- Windows/AddClientWindow.xaml.cs
+++ Windows/AddClientWindow.xaml.cs
@@ -57,5 +57,134 @@ namespace EquipmentRent3ISP9_7.Windows
             // Сохраняем employee для доступа вне конструктора
             editClient = client;
         }
-    }
+
+        private void btnAddNew_Click(object sender, RoutedEventArgs e)
+        {
+            #region Validation
+            // Null or White Space
+            if (string.IsNullOrWhiteSpace(txtLname.Text))
+            {
+                MessageBox.Show("Поле ФАМИЛИЯ пустое!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtLname.Text))
+            {
+                MessageBox.Show("Поле ИМЯ пустое!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtLname.Text))
+            {
+                MessageBox.Show("Поле EMAIL пустое!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtLname.Text))
+            {
+                MessageBox.Show("Поле ТЕЛЕФОН пустое!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(dpBirthday.Text))
+            {
+                MessageBox.Show("Поле ДАТА РОЖДЕНИЯ пустое!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtPassport.Text))
+            {
+                MessageBox.Show("Поле ПАСПОРТ пустое!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Incorrect Data
+            //var authUser = HelperClass.HelperCl.Context.Employee.ToList().
+            //    Where(i => i.Login == txtLogin.Text).FirstOrDefault();
+
+            //if (authUser != null && isEdit == false)
+            //{
+            //    MessageBox.Show("Данный логин уже занят!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            //    return;
+            //}
+
+            if (!long.TryParse(txtPhone.Text, out long res))
+            {
+                MessageBox.Show("Поле ТЕЛЕФОН введено некорректно!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            bool IsValidEmail(string email)
+            {
+                string pattern = "[.\\-_a-z0-9]+@([a-z0-9][\\-a-z0-9]+\\.)+[a-z]{2,6}";
+                Match isMatch = Regex.Match(email, pattern, RegexOptions.IgnoreCase);
+                return isMatch.Success;
+            }
+
+            if (IsValidEmail(txtEmail.Text) == false)
+            {
+                MessageBox.Show("Введен некорректный Email", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            #endregion
+
+            if (isEdit == true)
+            {
+                // Обработка случайного нажатия
+                var resClick = MessageBox.Show("Изменить пользователя?", "Подтверждение редактирования", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (resClick == MessageBoxResult.No)
+                { return; }
+
+                try
+                {
+                    editClient.LastName = txtLname.Text;
+                    editClient.FirstName = txtFname.Text;
+                    editClient.MiddleName = txtMname.Text;
+                    editClient.IdGender = (cmbGender.SelectedItem as Gender).IdGender;
+                    editClient.Email = txtEmail.Text;
+                    editClient.Phone = txtPhone.Text;
+                    editClient.Birthday = dpBirthday.Text;
+                    editClient.Passport = txtPassport.Text;
+
+                    HelperClass.HelperCl.Context.SaveChanges();
+                    MessageBox.Show("Пользователь изменён!", "Редактирование");
+                    Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), "Ошибка!");
+                }
+            }
+            else
+            {
+                // Обработка случайного нажатия
+                var resClick = MessageBox.Show("Добавить пользователя?", "Подтверждение добавления", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (resClick == MessageBoxResult.No)
+                { return; }
+
+                try
+                {
+                    Client newClient = new Client
+                    {
+                        LastName = txtLname.Text,
+                        FirstName = txtFname.Text,
+                        MiddleName = txtMname.Text,
+                        IdGender = (cmbGender.SelectedItem as Gender).IdGender,
+                        Email = txtEmail.Text,
+                        Phone = txtPhone.Text,
+                        Birthday = dpBirthday.Text;
+                        
+                    };
+
+                    HelperClass.HelperCl.Context.Employee.Add(newEmployee);
+                    HelperClass.HelperCl.Context.SaveChanges();
+                    MessageBox.Show("Пользователь добавлен!", "Добавление");
+                    Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), "Ошибка!");
+                }
+            }
+        }
 }
